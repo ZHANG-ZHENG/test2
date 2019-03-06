7247cfc67bbcc8aec8d8c70306d3fb4afc6b0089
zhengzhang <zhengzhang@ruijie.com.cn>
2019-03-06 13:32:40 +0800

    thread-test

---
+++ thread-test/src/main/java/com/atomic/AtomicReferenceTest.java
@@ -0,0 +1,26 @@
+package com.atomic;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class AtomicReferenceTest {
+    public final static AtomicReference<String> attxnicStr = new AtomicReference<String>("abc");
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread() {
+                public void run() {
+                    try {
+                        Thread.sleep(Math.abs((int) (Math.random() * 100)));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (attxnicStr.compareAndSet("abc", "def")) {
+                        System.out.println("Thread:" + Thread.currentThread().getId() + " change value to " + attxnicStr.get());
+                    } else {
+                        System.out.println("Thread:" + Thread.currentThread().getId() + " change failed!");
+                    }
+                }
+            }.start();
+        }
+    }
+}
