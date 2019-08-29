0e5adf293e4af7a6090851d5474a37599f82bfd9
zhengzhang <zhengzhang@ruijie.com.cn>
2019-08-29 16:12:12 +0800

    quartz

--- quartz/src/main/java/top/zhost/QuartzDemo1.java
+++ quartz/src/main/java/top/zhost/QuartzDemo1.java
@@ -4,10 +4,15 @@ import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 /**
  * 任务类
  */
 public class QuartzDemo1 implements Job{
+	
+	public static Integer count = 0;
+	
     /**
      * 任务被触发时执行的方法
      */
@@ -19,6 +24,18 @@ public class QuartzDemo1 implements Job{
             taskId = dataMap.getLong("TASK_ID");
           
         }
-        System.out.println("你收到消息了。。。"+taskId);
+
+        count++;
+        
+        Scheduler scheduler = context.getScheduler();
+        if(count == 3){
+        	try {
+				scheduler.unscheduleJob("triggerName", "triggerGroup");
+			} catch (SchedulerException e) {
+				e.printStackTrace();
+			}
+        }
+        
+        System.out.println(taskId+ "你收到消息了。。。 "+count +" "+context.getTrigger().getNextFireTime());
     }
 }
--- quartz/src/main/java/top/zhost/TestQuartz1.java
+++ quartz/src/main/java/top/zhost/TestQuartz1.java
@@ -1,6 +1,7 @@
 package top.zhost;
 
 import java.text.ParseException;
+import java.util.Date;
 
 import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
@@ -15,16 +16,22 @@ public class TestQuartz1 {
         //3创建Scheduler(任务调度)对象
         try {
             Scheduler scheduler=StdSchedulerFactory.getDefaultScheduler();  
+            System.out.println(scheduler.isStarted());
             scheduler.start();
+            System.out.println(scheduler.isStarted());
+
             
             Trigger trigger = new CronTrigger("triggerName", "triggerGroup", "jobName", "jobGroup", "0/4 * * * * ?");
             trigger.getJobDataMap().put("TASK_ID", 1000L);
+            trigger.getJobDataMap().put("count", 0);
             JobDetail jobDetail = new JobDetail("jobName", "jobGroup",QuartzDemo1.class);    
             scheduler.scheduleJob(jobDetail,trigger);
             
             Trigger trigger2 = new CronTrigger("triggerName2", "triggerGroup", "jobName2", "jobGroup", "0/2 * * * * ?");
             JobDetail jobDetail2 = new JobDetail("jobName2", "jobGroup",QuartzDemo2.class);    
-            scheduler.scheduleJob(jobDetail2,trigger2);
+            Date nextExecutionTime = scheduler.scheduleJob(jobDetail2,trigger2);
+            System.out.println(nextExecutionTime);
+
             
             //停止
             Trigger triggerz = scheduler.getTrigger("triggerName", "triggerGroup");
@@ -35,6 +42,10 @@ public class TestQuartz1 {
 
             
             scheduler.shutdown();
+            
+//            // Trigger已存在，那么更新相应的定时设置
+//            ((CronTrigger) trigger).setCronExpression(task.getOptCron());
+//            nextExecutionTime = operatorScheduler.rescheduleJob(trigger.getName(), trigger.getGroup(), trigger);
         } catch (Exception e) {
             e.printStackTrace();
         }
